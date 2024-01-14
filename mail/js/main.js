1ba5883a9d636065a3b29d0c07f20c652fd65464
lucapolzone <lucapolzone@gmail.com>
2024-01-14 16:33:20 +0100

    feat: add button function

--- mail/js/main.js
+++ mail/js/main.js
@@ -12,3 +12,20 @@ const checkButton = document.getElementById("login");
 // Feedback
 const feedback = document.getElementById("feedback");
 
+//il bottone verifica se la mail è registrata
+checkButton.addEventListener("click", function () {
+  
+  // Input element
+  const mailUser = document.getElementById("mail-user").value;
+
+  for (let i = 0; i < mailList.length; i++) {
+    if (mailUser == mailList[i]) {
+      console.log("Mail trovata");
+      feedback.innerText = `La tua mail risulta registrata`;
+      break;
+    } else {
+      console.log("Mail NON trovata");
+      feedback.innerText = `La tua mail NON risulta registrata`;
+    }
+  }
+});
