0c9a8aee2f59fcb7b6a57d2e69dcdd84e2eb009b
lucapolzone <lucapolzone@gmail.com>
2024-01-14 18:15:35 +0100

    feat: add variables for html elements

---
+++ dadi/js/main.js
@@ -0,0 +1,6 @@
+// Richiamo l'elemento html "Bottone"
+const playButton = document.getElementById("play-button");
+
+// Richiamo l'elemento l'elemento html "Risultato partita"
+const gameResult = document.getElementById("game-result");
+
