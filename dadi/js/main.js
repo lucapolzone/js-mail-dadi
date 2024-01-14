5afa15bf10b6e90f512b36569d167287eac35dd3
lucapolzone <lucapolzone@gmail.com>
2024-01-14 18:16:07 +0100

    feat: add button function

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -4,3 +4,22 @@ const playButton = document.getElementById("play-button");
 // Richiamo l'elemento l'elemento html "Risultato partita"
 const gameResult = document.getElementById("game-result");
 
+// Creo una funzione sul bottone
+playButton.addEventListener("click", function() {
+
+  //Genero un numero randomico da 1 a 6 per il giocatore
+  let playerScore = Math.floor(Math.random() * 6 + 1);
+  console.log(playerScore);
+  
+  //Genero un numero randomico da 1 a 6 per il computer
+  let pcScore = Math.floor(Math.random() * 6 + 1);
+  console.log(pcScore);
+
+  if(playerScore > pcScore) {
+    gameResult.innerText = `HAI VINTO! Hai fatto ${playerScore}, il computer ha fatto ${pcScore}`;
+  } else if (playerScore < pcScore) {
+    gameResult.innerText = `HAI PERSO! Il computer ha fatto ${pcScore}, tu hai fatto solo ${playerScore}`;
+  } else {
+    gameResult.innerText = `PAREGGIO`;
+  }
+});
