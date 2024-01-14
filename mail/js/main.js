cf3abe2b038adb8ab7dced6bf586223ce2e62938
lucapolzone <lucapolzone@gmail.com>
2024-01-14 16:32:15 +0100

    feat: add variables for html elements

---
+++ mail/js/main.js
@@ -0,0 +1,14 @@
+const mailList = [
+  "mario.rossi@email.com",
+  "mario.bianchi@email.com",
+  "nicolo.pincopallo@email.com",
+  "sara.esempio@email.com",
+  "giacomo.cognome@email.com",
+];
+
+//Bottone Accedi
+const checkButton = document.getElementById("login");
+
+// Feedback
+const feedback = document.getElementById("feedback");
+
